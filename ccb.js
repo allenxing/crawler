72f7c2ebe99a27d809588c27374f04aea9432948
allenxing <xinghainan1226@gmail.com>
2017-01-22 14:58:59 +0800

    complete ccb

--- ccb.js
+++ ccb.js
@@ -11,49 +11,34 @@ let db = require('./db.js');
 let config = require('./config.js').ccb;
 let ep = eventproxy();
 let flUlrs = [];
-let crwaler = () => {
-	console.log('ccb crawler begin..............');
-	config.citys.forEach((province, index) => {
-		if (province.code != 1020) return;
-		province.city.forEach((city, i) => {
-			console.log(city.name);
-			flUlrs.push({
-					url: 'http://creditcard.ccb.com/webtran/get_crd_info.gsp' + '?card_province=' + province.code + '&card_city=' + city.code + '&table_type=2',
-					city: city.name
-				})
-				// page3(0, 10, province.code, city);
-		})
+let saveFirstErrorUrls = (url) => {
+	fs.writeFile("firstErrorUrls.js", url + '\n', {
+		flag: 'a'
+	}, function(err) {
+		if (!err)
+			console.log("firstErrorUrls.js写入成功！")
 	})
-	console.log('一级url', flUlrs.length)
-	async.mapLimit(flUlrs, 10000, function(item, callback) {
-		fetchSecondUrl(item, callback);
-	}, function(err, result) {
-		console.log('=========== result: ===========');
-		let secondUrls = [];
-		result.forEach((it, index) => {
-			secondUrls = secondUrls.concat(it)
-		})
-		console.log('二级url:', secondUrls.length);
-		fs.writeFile("secondUrls.js", JSON.stringify(secondUrls), function(err) {
-			if (!err)
-				console.log("写入成功！")
-		})
-		getDetails(secondUrls)
-	});
 }
-
-function getDetails(secondUrls) {
-	async.mapLimit(secondUrls, 4000, function(it, callback) {
+let saveSecondErrorUrls = (url) => {
+	fs.writeFile("secondErrorUrls.js", url + '\n', {
+		flag: 'a'
+	}, function(err) {
+		if (!err)
+			console.log("secondErrorUrls.js写入成功！")
+	})
+}
+let getDetails = (secondUrls) => {
+	async.mapLimit(secondUrls, 100, function(it, callback) {
 		superagent.get(it.url)
 			.end((err, res) => {
-				if(err){
-					callback(err, 'error')
-					return;
-				}
-				if (res.ok) {
-					let $ = cheerio.load(res.text);
-					let id = 'ccb_' + it.id;
-					let item = {
+				if (err) {
+					callback(null, 'err')
+					saveSecondErrorUrls(it.url)
+				} else {
+					if (res) {
+						let $ = cheerio.load(res.text);
+						let id = 'ccb_' + it.id;
+						let item = {
 							_id: id,
 							shopname: $('.content-left').find('.thhd_head').text(),
 							tel: $('.content-left').find('.content').find('p').eq(4).text().replace('商户电话：', ''),
@@ -64,209 +49,80 @@ function getDetails(secondUrls) {
 							bank: 'ccb',
 							city: it.city
 						}
-						// console.log(item);
-						/*														db.Bank.findOneAndUpdate({
-																					_id: id
-																				}, item, function(err, doc) {
-																					if (doc === null) {
-																						db.Bank.create(item);
-																					}
-																				})*/
-					callback(null, item)
-				} else {
-					console.log('Oh no! error ' + res.text);
-					callback(err, 'error')
-					return;
-				}
-			})
-	}, function(err, result) {
-		console.log('活动数', result.length);
-	});
-}
-
-function fetchSecondUrl(item, callback) {
-	console.log(item.url)
-	superagent.get(item.url).end((err, resp) => {
-		let arr = [];
-		if (resp.ok) {
-			if (resp.text.match('totalNum')) {
-				resp.text.replace(/\"biz_id\"\:\"(\w+)\"/g, (a, biz_id) => {
-					arr.push({
-						url: 'http://creditcard.ccb.com/cn/creditcard/favorable/' + biz_id + '.html',
-						city: item.city,
-						id: biz_id
-					});
-					// superagent.get('http://creditcard.ccb.com/cn/creditcard/favorable/' + biz_id + '.html')
-					// 	.end((err, res) => {
-					// 		if (res.ok) {
-					// 			let $ = cheerio.load(res.text);
-					// 			let id = 'ccb_' + biz_id;
-					// 			let item = {
-					// 				_id: id,
-					// 				shopname: $('.content-left').find('.thhd_head').text(),
-					// 				tel: $('.content-left').find('.content').find('p').eq(4).text().replace('商户电话：', ''),
-					// 				address: $('.content-left').find('.content').find('p').eq(5).text().replace('商户地址：', ''),
-					// 				date: $('.content-left').find('.content').find('p').eq(3).text().replace('截止时间：', ''),
-					// 				desp: $('.content-left').find('.content').find('p').eq(2).text().replace('优惠信息：', ''),
-					// 				img: 'http://creditcard.ccb.com/' + $('.content-left').find('.thhd_head').next().attr('src'),
-					// 				bank: 'ccb',
-					// 				city: city.name
-					// 			}
-					// 			console.log(item);
-					// 			/*									db.Bank.findOneAndUpdate({
-					// 													_id: id
-					// 												}, item, function(err, doc) {
-					// 													if (doc === null) {
-					// 														db.Bank.create(item);
-					// 													}
-					// 												})*/
-					// 		} else {
-					// 			console.log('Oh no! error ' + res.text);
-					// 		}
-					// 	})
-				});
-				callback(null, arr)
-			}
-		} else {
-			console.log('Oh no! error ' + resp.text);
-			callback(err, url)
-		}
-	})
-}
-let page = (start, end, province, city) => {
-	if (city.code != 1468) return;
-	console.log(start + 1, end)
-	console.log('http://creditcard.ccb.com/webtran/get_crd_info.gsp' + '?card_province=' + province + '&card_city=' + city.code + '&startNum=' + (start + 1) + '&endNum=' + end + '&table_type=2')
-	superagent.get('http://creditcard.ccb.com/webtran/get_crd_info.gsp')
-		.query({
-			'card_province': province,
-			'card_city': city.code,
-			'startNum': start + 1,
-			'endNum': end,
-			'table_type': 2
-		})
-		.end((err, resp) => {
-			if (resp.ok) {
-				// console.log(res.text);
-				//由于返回的部分数据有问题,导致不能转换成json,这里采用字符串匹配
-				if (resp.text.match('totalNum')) {
-					resp.text.replace(/\"biz_id\"\:\"(\w+)\"/g, (a, biz_id) => {
-						superagent.get('http://creditcard.ccb.com/cn/creditcard/favorable/' + biz_id + '.html')
-							.end((err, res) => {
-								if (res.ok) {
-									let $ = cheerio.load(res.text);
-									let id = 'ccb_' + biz_id;
-									let item = {
-											_id: id,
-											shopname: $('.content-left').find('.thhd_head').text(),
-											tel: $('.content-left').find('.content').find('p').eq(4).text().replace('商户电话：', ''),
-											address: $('.content-left').find('.content').find('p').eq(5).text().replace('商户地址：', ''),
-											date: $('.content-left').find('.content').find('p').eq(3).text().replace('截止时间：', ''),
-											desp: $('.content-left').find('.content').find('p').eq(2).text().replace('优惠信息：', ''),
-											img: 'http://creditcard.ccb.com/' + $('.content-left').find('.thhd_head').next().attr('src'),
-											bank: 'ccb',
-											city: city.name
-										}
-										// console.log(item);
-									db.Bank.findOneAndUpdate({
-										_id: id
-									}, item, function(err, doc) {
-										if (doc === null) {
-											db.Bank.create(item);
-										}
-									})
-								} else {
-									console.log('Oh no! error ' + res.text);
-								}
-							})
-					});
-					page(end, end + config.step, province, city);
-				}
-			} else {
-				console.log('Oh no! error ' + resp.text);
-			}
-		})
-}
-let page1 = (start, end, province, city) => {
-	// if (city.code != 1468) return;
-	console.log(start + 1, end)
-	console.log('http://creditcard.ccb.com/webtran/get_crd_info.gsp' + '?card_province=' + province + '&card_city=' + city.code + '&startNum=' + (start + 1) + '&endNum=' + end + '&table_type=2')
-	request('http://creditcard.ccb.com/webtran/get_crd_info.gsp' + '?card_province=' + province + '&card_city=' + city.code + '&startNum=' + (start + 1) + '&endNum=' + end + '&table_type=2', function(error, response, body) {
-		if (body.match('totalNum')) {
-			body.replace(/\"biz_id\"\:\"(\w+)\"/g, (a, biz_id) => {
-				request('http://creditcard.ccb.com/cn/creditcard/favorable/' + biz_id + '.html', function(error, response, body) {
-					let $ = cheerio.load(body);
-					let id = 'ccb_' + biz_id;
-					let item = {
-							_id: id,
-							shopname: $('.content-left').find('.thhd_head').text(),
-							tel: $('.content-left').find('.content').find('p').eq(4).text().replace('商户电话：', ''),
-							address: $('.content-left').find('.content').find('p').eq(5).text().replace('商户地址：', ''),
-							date: $('.content-left').find('.content').find('p').eq(3).text().replace('截止时间：', ''),
-							desp: $('.content-left').find('.content').find('p').eq(2).text().replace('优惠信息：', ''),
-							img: 'http://creditcard.ccb.com/' + $('.content-left').find('.thhd_head').next().attr('src'),
-							bank: 'ccb',
-							city: city.name
-						}
-						// console.log(item);
-						/*db.Bank.findOneAndUpdate({
+						console.log(item);
+						db.Bank.findOneAndUpdate({
 							_id: id
 						}, item, function(err, doc) {
 							if (doc === null) {
 								db.Bank.create(item);
 							}
-						})*/
-				})
-			});
-			page1(end, end + config.step, province, city);
+						})
+						callback(null, item)
+					} else {
+						callback(null, {})
+						saveSecondErrorUrls(it.url)
+					}
+				}
+			})
+
+	}, function(err, result) {
+		console.log('活动总数:', result.length);
+		console.log('ccb crawler end..............');
+	});
+}
+let fetchSecondUrl = (item, callback) => {
+	superagent.get(item.url).end((err, resp) => {
+		let arr = [];
+		if (err) {
+			callback(null, 'error');
+			saveFirstErrorUrls(item.url)
+		} else {
+			if (resp) {
+				if (resp.text.match('totalNum')) {
+					resp.text.replace(/\"biz_id\"\:\"(\w+)\"/g, (a, biz_id) => {
+						arr.push({
+							url: 'http://creditcard.ccb.com/cn/creditcard/favorable/' + biz_id + '.html',
+							city: item.city,
+							id: biz_id
+						});
+					});
+					callback(null, arr)
+				} else {
+					callback(null, arr)
+					saveFirstErrorUrls(item.url)
+				}
+			} else {
+				callback(null, url)
+				saveFirstErrorUrls(item.url)
+			}
 		}
 	})
 }
-let page3 = (a, b, province, city) => {
-	superagent.get('http://creditcard.ccb.com/webtran/get_crd_info.gsp')
-		.query({
-			'card_province': province,
-			'card_city': city.code,
-			'table_type': 2
+let crwaler = () => {
+	console.log('ccb crawler begin..............');
+	config.citys.forEach((province, index) => {
+		province.city.forEach((city, i) => {
+			flUlrs.push({
+				url: 'http://creditcard.ccb.com/webtran/get_crd_info.gsp' + '?card_province=' + province.code + '&card_city=' + city.code + '&table_type=2',
+				city: city.name
+			})
 		})
-		.end((err, resp) => {
-			if (resp.ok) {
-				if (resp.text.match('totalNum')) {
-					resp.text.replace(/\"biz_id\"\:\"(\w+)\"/g, (a, biz_id) => {
-						superagent.get('http://creditcard.ccb.com/cn/creditcard/favorable/' + biz_id + '.html')
-							.end((err, res) => {
-								if (res.ok) {
-									let $ = cheerio.load(res.text);
-									let id = 'ccb_' + biz_id;
-									let item = {
-										_id: id,
-										shopname: $('.content-left').find('.thhd_head').text(),
-										tel: $('.content-left').find('.content').find('p').eq(4).text().replace('商户电话：', ''),
-										address: $('.content-left').find('.content').find('p').eq(5).text().replace('商户地址：', ''),
-										date: $('.content-left').find('.content').find('p').eq(3).text().replace('截止时间：', ''),
-										desp: $('.content-left').find('.content').find('p').eq(2).text().replace('优惠信息：', ''),
-										img: 'http://creditcard.ccb.com/' + $('.content-left').find('.thhd_head').next().attr('src'),
-										bank: 'ccb',
-										city: city.name
-									}
-									console.log(item);
-									/*									db.Bank.findOneAndUpdate({
-																			_id: id
-																		}, item, function(err, doc) {
-																			if (doc === null) {
-																				db.Bank.create(item);
-																			}
-																		})*/
-								} else {
-									console.log('Oh no! error ' + res.text);
-								}
-							})
-					});
-				}
-			} else {
-				console.log('Oh no! error ' + resp.text);
-			}
+	})
+	console.log('一级url', flUlrs.length)
+	async.mapLimit(flUlrs, 10, function(item, callback) {
+		fetchSecondUrl(item, callback);
+	}, function(err, result) {
+		let secondUrls = [];
+		result.forEach((it, index) => {
+			secondUrls = secondUrls.concat(it)
 		})
+		console.log('二级url:', secondUrls.length);
+		fs.writeFile("secondUrls.js", JSON.stringify(secondUrls), function(err) {
+			if (!err)
+				console.log("secondUrls.js写入成功！")
+		})
+		getDetails(secondUrls)
+	});
 }
 module.exports = {
 	run: crwaler
